206d3d21610dde0c2b62b95bf37cdbab2a3aef37
secret-point <secret.point2020@gmail.com>
2021-12-07 07:03:16 -0800

    trying to output defaultValues

--- src/components/UpdateDialog.tsx
+++ src/components/UpdateDialog.tsx
@@ -161,6 +161,7 @@ const UpdateDialog: FC<UpdateDialogProps> = ({
   });
 
   useEffect(() => {
+    console.log(methods.reset, defaultValues);
     methods.reset({ defaultValues });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [methods.reset, defaultValues]);
